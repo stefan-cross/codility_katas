88524569582938eb084c5359627dd641bd38886f
stefan <ssorcnafets@gmail.com>
2021-07-19 21:26:11 +0100

    adding ttd tests for zero case

--- src/main/kotlin/com/opentuned/binarygap/BinaryGap.kt
+++ src/main/kotlin/com/opentuned/binarygap/BinaryGap.kt
@@ -2,6 +2,6 @@ package com.opentuned.binarygap
 
 class BinaryGap {
     fun solution(N: Int) : Int {
-        return N.toString(2).substringAfter("1").substringBefore("1").length
+        return N.toString(2).substringAfter("1", "").substringBeforeLast("1", "").length
     }
 }
--- src/test/kotlin/com/opentuned/binarygap/BinaryGapTest.kt
+++ src/test/kotlin/com/opentuned/binarygap/BinaryGapTest.kt
@@ -1,32 +1,47 @@
 package com.opentuned.binarygap
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 
 internal class BinaryGapTest {
 
+    private val underTest = BinaryGap()
+
+    @Tag("tdd driven test")
+    @Test
+    internal fun `zero case`() {
+        assertEquals(0, underTest.solution(0))
+    }
+
+
+    @Tag("provided test")
     @Test
     internal fun `number 9 has binary representation 1001 and contains a binary gap of length 2`() {
-        assertEquals(2, BinaryGap().solution(9))
+        assertEquals(2, underTest.solution(9))
     }
 
+    @Tag("provided test")
     @Test
     internal fun `number 529 has binary representation 1000010001 and contains the largest binary gap and has a length 4`() {
-        assertEquals(4, BinaryGap().solution(529))
+        assertEquals(4, underTest.solution(529))
     }
 
+    @Tag("provided test")
     @Test
     internal fun `number 20 has binary representation 10100 and contains one binary gap of length 1`() {
-        assertEquals(1, BinaryGap().solution(20))
+        assertEquals(1, underTest.solution(20))
     }
 
+    @Tag("provided test")
     @Test
     internal fun `number 15 has binary representation 1111 and has no binary gaps`() {
-        assertEquals(0, BinaryGap().solution(15))
+        assertEquals(0, underTest.solution(15))
     }
 
+    @Tag("provided test")
     @Test
     internal fun `number 32 has binary representation 100000 and has no binary gaps`() {
-        assertEquals(0, BinaryGap().solution(32))
+        assertEquals(0, underTest.solution(32))
     }
 }
