b7a1f9624063aa2e3fd59e40a131bf855291b777
stefan <ssorcnafets@gmail.com>
2021-07-22 20:36:58 +0100

    adding more test cases and implement corner cases

--- src/main/kotlin/com/opentuned/cyclicrotation/CyclicRotation.kt
+++ src/main/kotlin/com/opentuned/cyclicrotation/CyclicRotation.kt
@@ -1,5 +1,6 @@
 package com.opentuned.cyclicrotation
 
 fun solution(A: IntArray, K: Int): IntArray {
-    return intArrayOf(*A.sliceArray(K-1..A.size-1), *A.sliceArray(0..K-2))
+    return if(A.size <= K || K < 0 ) A
+    else intArrayOf(*A.sliceArray(K-1..A.size-1), *A.sliceArray(0..K-2))
 }
--- src/test/kotlin/com/opentuned/cyclicrotation/CyclicRotationKtTest.kt
+++ src/test/kotlin/com/opentuned/cyclicrotation/CyclicRotationKtTest.kt
@@ -2,13 +2,18 @@ package com.opentuned.cyclicrotation
 
 import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 
 internal class CyclicRotationKtTest {
 
     @Test
     fun `given an array A consisting of N integers and an integer K, returns the array A rotated K times`() {
-        val A = intArrayOf(3, 8, 9, 7, 6)
-        val K = 3
-        assertArrayEquals(intArrayOf(9, 7, 6, 3, 8), (solution(A, K)))
+        assertAll("intArrays can be rotated K times",
+            { assertArrayEquals(intArrayOf(0, 0, 0), (solution(intArrayOf(0, 0, 0), 1))) },
+            { assertArrayEquals(intArrayOf(), (solution(intArrayOf(), 1))) },
+            { assertArrayEquals(intArrayOf(), (solution(intArrayOf(), -1))) },
+            { assertArrayEquals(intArrayOf(1, 2, 3, 4), (solution(intArrayOf(1, 2, 3, 4), 4))) },
+            { assertArrayEquals(intArrayOf(9, 7, 6, 3, 8), (solution(intArrayOf(3, 8, 9, 7, 6), 3))) },
+        )
     }
 }
