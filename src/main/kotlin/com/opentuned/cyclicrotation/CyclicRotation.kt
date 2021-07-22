b46915423e47afca3ea675ef74497e2d4351961b
stefan <ssorcnafets@gmail.com>
2021-07-22 20:20:44 +0100

    first pass at implementation

--- src/main/kotlin/com/opentuned/cyclicrotation/CyclicRotation.kt
+++ src/main/kotlin/com/opentuned/cyclicrotation/CyclicRotation.kt
@@ -1,5 +1,5 @@
 package com.opentuned.cyclicrotation
 
 fun solution(A: IntArray, K: Int): IntArray {
-    return A
+    return intArrayOf(*A.sliceArray(K-1..A.size-1), *A.sliceArray(0..K-2))
 }
--- src/test/kotlin/com/opentuned/cyclicrotation/CyclicRotationKtTest.kt
+++ src/test/kotlin/com/opentuned/cyclicrotation/CyclicRotationKtTest.kt
@@ -1,14 +1,14 @@
 package com.opentuned.cyclicrotation
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Test
 
 internal class CyclicRotationKtTest {
 
     @Test
-    internal fun `given an array A consisting of N integers and an integer K, returns the array A rotated K times`() {
+    fun `given an array A consisting of N integers and an integer K, returns the array A rotated K times`() {
         val A = intArrayOf(3, 8, 9, 7, 6)
         val K = 3
-        assertEquals(intArrayOf(9, 7, 6, 3, 8), solution(A, K))
+        assertArrayEquals(intArrayOf(9, 7, 6, 3, 8), (solution(A, K)))
     }
 }
